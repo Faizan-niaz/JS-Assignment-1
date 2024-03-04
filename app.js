9f6fc286f3e553227ca88c1d5cc76a3afb82f8e6
Faizan-niaz <faizan22niaz@gmail.com>
2024-03-05 00:49:25 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+
+// 1. Write a script to greet your website visitor using JS alert
+// box.
+// alert("Greeting Dear Visitor:")
+
+// 2. Write a script to display following message on your web
+// page:
+// alert("Error! Please enter a valid password")
+
+// 3. Write a script to display following message on your web
+// page: (Hint : Use line break)
+// alert("Welcome to JS land... \nHappy Coding!")
+
+// 4. Write a script to display following messages in sequence:
+// alert("Welcome to JS land")
+// alert("Happy Coding! \nPrevent this page from creating additional dialogs.")
+
+// 5. Generate the following message through browser’s
+// developer console:
+// console.log(alert("Hello.. I can run JS through my web browser's console"))
